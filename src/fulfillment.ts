cac6538b355a9200785b240efe5ec35af3d9ebe2
Yoichiro Tanaka <yoichiro6642@gmail.com>
2020-03-07 21:45:30 +0900

    Support English.

--- src/fulfillment.ts
+++ src/fulfillment.ts
@@ -1,47 +1,105 @@
-import { BasicCard, BrowseCarousel, BrowseCarouselItem, Button, dialogflow, SimpleResponse } from 'actions-on-google';
+import {
+  BasicCard,
+  BrowseCarousel,
+  BrowseCarouselItem,
+  Button,
+  dialogflow,
+  DialogflowConversation,
+  SimpleResponse
+} from 'actions-on-google';
 import { Database } from './database';
 import * as moment from 'moment';
+import * as i18n from 'i18n';
+import { sprintf } from 'sprintf-js';
 
 const SITE_URL = 'https://www.mhlw.go.jp';
 
 const app = dialogflow();
 const database = new Database();
 
+i18n.configure({
+  locales: ["en-US", "ja-JP"],
+  directory: __dirname + "/../locales",
+  defaultLocale: "ja-JP"
+});
+
+const _setupLocale = (conv: DialogflowConversation<unknown, unknown>): void => {
+  console.log(conv.user.locale);
+  i18n.setLocale(conv.user.locale);
+};
+
+const _sprintf = (message: string, params: any[]): string => {
+  if (params.length > 0) {
+    return sprintf(message, ...params);
+  } else {
+    return message;
+  }
+};
+
+const _i18n = (name: string, ...params: any[]): string => {
+  const message = i18n.__(name);
+  if (Array.isArray(message)) {
+    return _sprintf(message[Math.floor(Math.random() * message.length)], params);
+  } else {
+    return _sprintf(message, params);
+  }
+};
+
 app.intent('Default Welcome Intent', async (conv) => {
+  _setupLocale(conv);
   const link = await database.fetchLatestLink();
   if (link) {
     const total = await database.fetchTotal(link);
     if (total) {
       const date = createDatePhrase(convertDate(link.date), moment());
-      if (conv.screen) {
-        conv.ask(new SimpleResponse({
-          speech: '厚生労働省が発表した新型コロナウイルスの国内での事例数をお知らせいたします。' +
-            'チャーター便帰国者は含まれていません。' +
-            `${date}時点での、国内を居住地とする事例数は、${total}件です。`,
-          text: '厚生労働省が発表した新型コロナウイルスの国内での事例数をお知らせいたします。'
-        }));
-        conv.ask(new BasicCard({
-          title: `国内の事例数: ${total}件`,
-          subtitle: `${date} 時点`,
-          text: '厚生労働省発表（チャーター便帰国者は含まれていません）\n日本国内を居住地とする事例の数です。',
-          buttons: new Button({
-            title: '厚生労働省ホームページ',
-            url: `${SITE_URL}${link.href}`
-          }),
-        }));
-        conv.ask('知りたい都道府県名をどうぞ。');
+      if (conv.user.locale === 'en-US') {
+        if (conv.screen) {
+          conv.close(new SimpleResponse({
+            speech: _i18n('WELCOME_MESSAGE_SPEECH_1', total, date),
+            text: _i18n('WELCOME_MESSAGE_TEXT_1')
+          }));
+          conv.close(new BasicCard({
+            title: _i18n('WELCOME_MESSAGE_BASIC_CARD_TITLE', total),
+            subtitle: _i18n('WELCOME_MESSAGE_BASIC_CARD_SUBTITLE', date),
+            text: _i18n('WELCOME_MESSAGE_BASIC_CARD_TEXT'),
+            buttons: new Button({
+              title: _i18n('WELCOME_MESSAGE_BASIC_CARD_BUTTON'),
+              url: `${SITE_URL}/english`
+            }),
+          }));
+        } else {
+          conv.close(
+            _i18n('WELCOME_MESSAGE_SPEECH_2', total, date)
+          );
+        }
+        conv.close(_i18n('END_MESSAGE_1') + _i18n('END_MESSAGE_2'));
       } else {
-        conv.ask(
-          '厚生労働省が発表した新型コロナウイルスの国内での事例数をお知らせいたします。' +
-          'チャーター便帰国者は含まれていません。' +
-          `${date}時点での、国内を居住地とする事例数は、${total}件です。` +
-          '知りたい都道府県名をどうぞ。'
-        );
+        if (conv.screen) {
+          conv.ask(new SimpleResponse({
+            speech: _i18n('WELCOME_MESSAGE_SPEECH_1', date, total),
+            text: _i18n('WELCOME_MESSAGE_TEXT_1')
+          }));
+          conv.ask(new BasicCard({
+            title: _i18n('WELCOME_MESSAGE_BASIC_CARD_TITLE', total),
+            subtitle: _i18n('WELCOME_MESSAGE_BASIC_CARD_SUBTITLE', date),
+            text: _i18n('WELCOME_MESSAGE_BASIC_CARD_TEXT'),
+            buttons: new Button({
+              title: _i18n('WELCOME_MESSAGE_BASIC_CARD_BUTTON'),
+              url: `${SITE_URL}${link.href}`
+            }),
+          }));
+          conv.ask(_i18n('WELCOME_MESSAGE_ASK'));
+        } else {
+          conv.ask(
+            _i18n('WELCOME_MESSAGE_SPEECH_2', date, total) +
+            _i18n('WELCOME_MESSAGE_ASK')
+          );
+        }
       }
       return;
     }
   }
-  conv.close('申し訳ございません。情報がありません。');
+  conv.close(_i18n('WELCOME_MESSAGE_NO_INFORMATION'));
 });
 
 const convertDate = (date: number): moment.Moment => {
@@ -55,17 +113,18 @@ const convertDate = (date: number): moment.Moment => {
 
 export const createDatePhrase = (date: moment.Moment, today: moment.Moment): string => {
   if (today.year() !== date.year()) {
-    return date.format('YYYY年M月D日');
+    return date.format(_i18n('DATE_FORMAT_1'));
   } else {
     if (today.isSame(date, 'date')) {
-      return '本日' + date.format('M月D日');
+      return _i18n("TODAY") + date.format(_i18n('DATE_FORMAT_2'));
     } else {
-      return date.format('M月D日');
+      return date.format(_i18n('DATE_FORMAT_2'));
     }
   }
 };
 
 app.intent('prefecture', async (conv, { prefecture }) => {
+  _setupLocale(conv);
   if (prefecture) {
     const link = await database.fetchLatestLink();
     if (link) {
@@ -74,80 +133,78 @@ app.intent('prefecture', async (conv, { prefecture }) => {
       if (people) {
         if (conv.screen) {
           conv.ask(new SimpleResponse({
-            speech: `${date}時点での、居住地が${prefecture}の事例数は、${people}件です。`,
-            text: `居住地が${prefecture}の事例数です。`
+            speech: _i18n('PREFECTURE_SPEECH_1', date, prefecture, people),
+            text: _i18n('PREFECTURE_TEXT_1', prefecture)
           }));
           conv.ask(new BasicCard({
-            title: `${prefecture}の事例数: ${people}件`,
-            subtitle: `${date} 時点`,
-            text: `厚生労働省発表（チャーター便帰国者は含まれていません）\n${prefecture}を居住地とする事例の数です。`,
+            title: _i18n('PREFECTURE_BASIC_CARD_TITLE', prefecture, people),
+            subtitle: _i18n('PREFECTURE_BASIC_CARD_SUBTITLE', date),
+            text: _i18n('PREFECTURE_BASIC_CARD_TEXT', prefecture),
             buttons: new Button({
-              title: '厚生労働省ホームページ',
+              title: _i18n('PREFECTURE_BASIC_CARD_BUTTON'),
               url: `${SITE_URL}${link.href}`
             }),
           }));
           conv.ask(
-            '他に知りたい都道府県名をどうぞ。'
+            _i18n('PREFECTURE_ASK')
           );
         } else {
           conv.ask(
-            `${date}時点での、居住地が${prefecture}の事例数は、${people}件です。` +
-            '他に知りたい都道府県名をどうぞ。'
+            _i18n('PREFECTURE_SPEECH_2', date, prefecture, people) +
+            _i18n('PREFECTURE_ASK')
           );
         }
       } else {
         conv.ask(
-          `${date}時点での、居住地が${prefecture}の事例数はありません。`
+          _i18n('PREFECTURE_SPEECH_3', date, prefecture)
         );
         conv.ask(
-          '他に知りたい都道府県名をどうぞ。'
+          _i18n('PREFECTURE_ASK')
         );
       }
     } else {
-      conv.close('申し訳ございません。情報がありません。');
+      conv.close(_i18n('PREFECTURE_NO_INFORMATION'));
     }
   } else {
-    conv.ask('もう一度、知りたい都道府県名を教えてください。');
+    conv.ask(_i18n('PREFECTURE_SPEECH_4'));
   }
 });
 
 app.intent('help', conv => {
+  _setupLocale(conv);
   conv.ask(
-    'これは、厚生労働省が発表した居住地別の新型コロナウイルスの事例数をお伝えするアクションです。' +
-    '厚生労働省のホームページで公開された報告数に基づいています。' +
-    'チャーター便帰国者は含まれていません。' +
-    '事例数を知りたい都道府県名をどうぞ。'
+    _i18n('HELP')
   );
 });
 
 app.intent('end', conv => {
+  _setupLocale(conv);
   if (conv.screen && conv.surface.capabilities.has('actions.capability.WEB_BROWSER')) {
     conv.close(
-      'マスクの着用や手洗いの徹底など、通常の感染症対策に努めてください。'
+      _i18n('END_MESSAGE_1')
     );
     conv.close(new BrowseCarousel({
       items: [
         new BrowseCarouselItem({
-          title: '新型コロナウイルス感染症について',
+          title: _i18n('END_BROWSE_CAROUSEL_1'),
           url: 'https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000164708_00001.html'
         }),
         new BrowseCarouselItem({
-          title: '厚生労働省 報道発表資料',
+          title: _i18n('END_BROWSE_CAROUSEL_2'),
           url: 'https://www.mhlw.go.jp/stf/houdou/index.html'
         }),
         new BrowseCarouselItem({
-          title: '厚生労働省ホームページ',
+          title: _i18n('END_BROWSE_CAROUSEL_3'),
           url: 'https://www.mhlw.go.jp/index.html'
         })
       ]
     }));
     conv.close(
-      'より詳しい情報は、厚生労働省のホームページをご覧ください。'
+      _i18n('END_MESSAGE_2')
     );
   } else {
     conv.close(
-      'マスクの着用や手洗いの徹底など、通常の感染症対策に努めてください。' +
-      'より詳しい情報は、厚生労働省のホームページをご覧ください。'
+      _i18n('END_MESSAGE_1') + _i18n('END_MESSAGE_2')
     );
   }
 });
